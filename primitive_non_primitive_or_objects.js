10f11fdeadee7862bf5304fc3196097c80922f61
Huraira faraz khan <hurairafaraz50@gmail.com>
2023-09-22 04:44:32 -0700

    7-datatypes

---
+++ primitive_non_primitive_or_objects.js
@@ -0,0 +1,19 @@
+let a = "huraira"
+let b = 45
+let c = true
+let d = BigInt("18")+BigInt("10")
+let e = Symbol()
+let f = null
+let g = undefined
+console.log(typeof a,b,c,d,)
+console.log(typeof e,f,g)
+const car =
+{
+    type:"Fiat",
+    model:"500",
+    color:"white"
+}
+console.log(car.model)
+console.log(car.type)
+console.log(car.color)
+console.log("my car type is "+ car.type + " and car model is " + car.model + " and my car color is " + car.color )
