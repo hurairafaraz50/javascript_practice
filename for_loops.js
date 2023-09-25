66b8cbecd47517e64c5c40078cb0b96d14c9ae52
Huraira faraz khan <hurairafaraz50@gmail.com>
2023-09-25 03:07:45 -0700

    some practice of loops

---
+++ for_loops.js
@@ -0,0 +1,20 @@
+//Write a program to print numbers from 1 to 10.
+// for ( let i=1 ; i<=10 ; i++){
+//     alert(i)
+// }
+
+// Write a program to calculate the sum of first 10 natural number.
+// let sum =0
+// for (let i=0 ; i<=10 ; i++)
+// {
+//     sum= sum +i
+// }
+// alert("the sum of 10 natural number " + sum)
+
+//Write a program that prompts the user to input a positive integer. It should then print the multiplication table of that number
+// const user = parseInt(prompt("Enter any number which you want a table : "))
+// for ( let i=1 ; i<=10 ; i++){ 
+//     const result=user*i
+//     alert(user+" X "+ i + " = " + result)
+// }
+
