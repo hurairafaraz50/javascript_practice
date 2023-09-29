0745a360489c4f3ec0cb4fa78b712b38786dd819
Huraira faraz khan <hurairafaraz50@gmail.com>
2023-09-29 05:41:07 -0700

    functions

---
+++ functions.js
@@ -0,0 +1,29 @@
+let num1 = parseFloat(prompt("ENTER FIRST NUMBER : "));
+let num2 = parseFloat(prompt("ENTER SECOND NUMBER : "));
+
+function mul(a, b) {
+  alert(a * b);
+}
+mul(num1, num2);
+function sub(a, b) {
+  alert(a - b);
+}
+sub(num1, num2);
+function add(a, b) {
+  alert(a + b);
+}
+add(num1, num2);
+function div(a, b) {
+  alert(a / b);
+}
+div(num1, num2);
+function mod(a, b) {
+  alert(a % b);
+}
+mod(num1, num2);
+
+function calculator(x,y){
+    while(true){
+        if (x+y)
+    }
+}
