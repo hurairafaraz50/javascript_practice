42cff68af5308dcb52165958b5121d2b74ed6de1
Huraira faraz khan <hurairafaraz50@gmail.com>
2023-09-30 22:34:09 -0700

    strings and escape sequence

---
+++ strings.js
@@ -0,0 +1,32 @@
+// let name = "huraira";
+// let friend = "uzair";
+// alert(name + " is friend of " + friend);
+// alert(name.length);
+// alert(friend.length);
+// alert(name[0]);
+// alert(name[1]);
+// alert(name[2]);
+// alert(name[3]);
+// alert(name[4]);
+// alert(name[5]);
+// alert(name[6]);
+
+//template litterals
+// let girl1 = "noor";
+// let girl2 = "adan";
+// let sentence;
+// sentence = `${girl1} is a friend of ${girl2}`;
+
+// alert(sentence);
+
+// escape sequence
+let v = "my\rname\ris\rhuraira\rfaraz\rkhan";
+let w = "my\tname\tis\thuraira\tfaraz\tkhan";
+let x = 'bana"na';
+let y = "bana'na";
+let z = "pine\napple";
+alert(v);
+alert(w);
+alert(x);
+alert(y);
+alert(z);
