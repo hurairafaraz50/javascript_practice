d8fe1863cf770f0950bd8988b5e5809568cb1137
Huraira faraz khan <hurairafaraz50@gmail.com>
2023-09-30 23:28:59 -0700

    different methods of strings

---
+++ string_methods.js
@@ -0,0 +1,18 @@
+// methods
+let name = " huraira faraz khan ";
+let friend = "rabi";
+let x = "                  mathmematics                           ";
+alert(name.length);
+alert(name.toUpperCase());
+alert(name.toLowerCase());
+alert(name.slice(2, 10));
+alert(name.slice(0));
+alert(name.replace("huraira", "uzair"));
+alert(name.concat("is a boyfriend of ", friend));
+alert(x.trim());
+
+// quiz:
+let y = "hamzakhan";
+for (let i = 0; i < y.length; i++) {
+  alert(y);
+}
--- strings.js
+++ strings.js
@@ -1,23 +1,23 @@
-// let name = "huraira";
-// let friend = "uzair";
-// alert(name + " is friend of " + friend);
-// alert(name.length);
-// alert(friend.length);
-// alert(name[0]);
-// alert(name[1]);
-// alert(name[2]);
-// alert(name[3]);
-// alert(name[4]);
-// alert(name[5]);
-// alert(name[6]);
+let name = "huraira";
+let friend = "uzair";
+alert(name + " is friend of " + friend);
+alert(name.length);
+alert(friend.length);
+alert(name[0]);
+alert(name[1]);
+alert(name[2]);
+alert(name[3]);
+alert(name[4]);
+alert(name[5]);
+alert(name[6]);
 
-//template litterals
-// let girl1 = "noor";
-// let girl2 = "adan";
-// let sentence;
-// sentence = `${girl1} is a friend of ${girl2}`;
+// template litterals
+let girl1 = "noor";
+let girl2 = "adan";
+let sentence;
+sentence = `${girl1} is a friend of ${girl2}`;
 
-// alert(sentence);
+alert(sentence);
 
 // escape sequence
 let v = "my\rname\ris\rhuraira\rfaraz\rkhan";
