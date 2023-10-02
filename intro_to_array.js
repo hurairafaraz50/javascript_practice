9a8e74fcb0fcaf6a95ed36a1b12423ae6f036e2e
Huraira faraz khan <hurairafaraz50@gmail.com>
2023-10-02 09:16:05 -0700

    arrays intro

---
+++ intro_to_array.js
@@ -0,0 +1,17 @@
+let class12 = [12, 534, 456, 234, false, true, "huraira", "khan", null, 32.4];
+alert(class12);
+alert(class12.length);
+class12[10] = 231; //undefined
+class12[15] = 31;
+alert(class12[0]);
+alert(class12[1]);
+alert(class12[2]);
+alert(class12[3]);
+alert(class12[4]);
+alert(class12[5]);
+alert(class12[6]);
+alert(class12[7]);
+alert(class12[8]);
+alert(class12[9]);
+alert(class12[10]);
+alert(class12[15]);
